dcf2d5dc19bc7378e9978c60c6d982abe905facc
koselevn <koselevn52@gmail.com>
2026-01-17 13:49:24 +0100

    dz-2-1

---
+++ src/models/note.js
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true
+    },
+    tag: {
+      type: String,
+      default: "Todo",
+      enum: ["Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"]
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Note = model('Note', noteSchema);
