f6a1db33bac53024fcf7e9fddec576affc20aa57
koselevn <koselevn52@gmail.com>
2026-02-15 14:20:58 +0100

    dz-5-3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -127,7 +127,7 @@ export const requestResetEmail = async (req, res) => {
   }
 
   res.status(200).json({
-    message: 'If this email exists, a reset link has been sent',
+    message: 'Password reset email sent successfully',
   });
 };
 
